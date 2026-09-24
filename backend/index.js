bce00f28f37663d46798f283f1f468f925786c89
sandy29l <sandyborn2904@gmail.com>
2026-09-24 17:50:03 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,126 @@
+const express= require("express");
+const cors=require("cors");
+const mongoose=require("mongoose");
+
+const app=express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb://127.0.0.1:27017/userdb").then(function(){
+    console.log("db success");
+}).catch(function(){
+    console.log("db fail");
+})
+
+const user= mongoose.model("user",{username:String , password:Number,activity: [String]},"user")
+
+/*user.find().then(function(retdata){
+    console.log(retdata);
+}).catch(function(){
+    console.log("error in retriving data");
+})*/
+
+app.get("/login",function(req,res){
+    user.find().then(function(retdata){
+        res.send(retdata);
+    })
+})
+
+app.post("/signup", function(req, res) {
+
+    const newusername = req.body.username;
+    const newpassword = req.body.password;
+
+    user.findOne({
+        $or: [
+            { username: newusername },
+            { password: newpassword }
+        ]
+    })
+    .then(function(existingUser) {
+
+        if (existingUser) {
+
+            if (existingUser.username === newusername) {
+                res.send("username already exists");
+            }
+            else if (existingUser.password === newpassword) {
+                res.send("password already exists");
+            }
+
+        }
+        else {
+
+            const new_user = new user({
+                username: newusername,
+                password: newpassword,
+                activity: []
+            });
+
+            new_user.save()
+                .then(function() {
+                    res.send("signup successful");
+                })
+                .catch(function() {
+                    res.send("error creating user");
+                });
+        }
+    })
+    .catch(function() {
+        res.send("error checking user");
+    });
+});
+
+app.post("/activity", function(req, res) {
+    const username = req.body.username;
+    const newActivity = req.body.activity;
+
+    user.updateOne(
+        { username: username },
+        { $push: { activity: newActivity } }
+    )
+    .then(function() {
+        res.send("Activity added successfully");
+    })
+    .catch(function() {
+        res.send("Error adding activity");
+    });
+});
+
+app.get("/getact", function(req, res) {
+    const username = req.query.username;
+
+    user.findOne({ username: username })
+        .select("activity -_id")
+        .then(function(retdata) {
+            // console.log(retdata);
+            if (!retdata) { return res.send([]); }
+             res.send(retdata.activity || []);
+            // res.send(retdata.activity);
+        })
+        .catch(function() {
+            res.send("Error");
+        });
+});
+
+app.delete("/activity", function(req, res) {
+
+    const username = req.body.username;
+    const activity = req.body.activity;
+
+    user.updateOne(
+        { username: username },
+        { $pull: { activity: activity } }
+    )
+    .then(function() {
+        res.send("Activity deleted successfully");
+    })
+    .catch(function() {
+        res.send("Error deleting activity");
+    });
+});
+
+app.listen(5000,function(){
+    console.log("Server started");
+})
